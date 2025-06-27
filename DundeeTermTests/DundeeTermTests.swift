6a07da97a0631e1fbdcc371a48b1bce244d0c7cc
dundeezhang <dundee_zhang@hotmail.com>
2025-06-26 23:59:35 -0400

    Initial Commit

---
+++ DundeeTermTests/DundeeTermTests.swift
@@ -0,0 +1,17 @@
+//
+//  DundeeTermTests.swift
+//  DundeeTermTests
+//
+//  Created by Dundee Zhang on 2025-06-26.
+//
+
+import Testing
+@testable import DundeeTerm
+
+struct DundeeTermTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
